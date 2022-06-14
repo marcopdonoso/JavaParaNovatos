3a08e88c755712b5e39e24022ed545e25a41c6e5
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-14 11:57:06 -0400

    Hola Mundo!

---
+++ Holamundo/src/Holamundo.java
@@ -0,0 +1,7 @@
+public class Holamundo {
+    /*Programa holamundo*/
+    public static void main(String[] args) {
+        //Este programa solo muestra un saludo por la pantalla
+        System.out.println("Hola, mundo!");
+    }
+}
