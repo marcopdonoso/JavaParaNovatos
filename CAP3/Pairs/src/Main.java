2a5bf082c11e646d06af9743366aa7130e4d312f
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-06-20 23:16:15 -0400

    Ejercicio 3.11: Pares o Nones. Adaptando el codigo del ejercicio anterior para que funcione de manera mas completa

--- CAP3/Pairs/src/Main.java
+++ CAP3/Pairs/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 0;
         int b = 0;
+        int sel = 0;
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Introduce el primer numero del rango");
@@ -23,11 +24,25 @@ public class Main {
             e.printStackTrace();
         }
 
-        Pairs pairs = new Pairs(a,b);
-        System.out.println("Pares");
-        pairs.pairs();
-        System.out.println("\nImpares");
-        pairs.odds();
+        System.out.println("Deseas ver los numeros Pares (1) o Impares (2)?");
+        try {
+            sel = Integer.parseInt(bufferedReader.readLine());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        Pairs pairs = new Pairs(a, b);
+
+        switch (sel) {
+            case 1:
+                pairs.pairs();
+                break;
+            case 2:
+                pairs.odds();
+                break;
+            default:
+                System.out.println("Opcion Invalida, programa terminado");
+        }
     }
 }
--- CAP3/Pairs/src/Pairs.java
+++ CAP3/Pairs/src/Pairs.java
@@ -6,31 +6,26 @@ public class Pairs {
     Pairs(int a, int b) {
         this.a = a;
         this.b = b;
+        if (this.a >= this.b) {
+            aux = this.a;
+            this.a = this.b;
+            this.b = aux;
+        }
     }
 
     public void pairs() {
-        if (a >= b) {
-            System.out.println("IDIOTA!!! el primer numero debe ser menor al segundo!!!, vuelve a nacer!!!");
-        } else {
-            for (int i = a + 1; i < b; i++) {
-                if (i % 2 == 0) {
-                    System.out.println(i);
-                }
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
             }
         }
     }
 
     public void odds() {
-        if (a >= b) {
-            aux = a;
-            a = b;
-            b = aux;
-        }
-        for (int i = a + 1; i < b; i++) {
+        for (int i = a; i <= b; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
-
     }
 }
