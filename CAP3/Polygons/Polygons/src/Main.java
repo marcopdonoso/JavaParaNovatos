280b9958ebc498c3e2632e6bf2801deb18fc04f6
Marco Perez Donoso <marcopdonoso@MacBook-Pro-de-Marco.local>
2022-07-05 15:02:14 -0400

    Ejercicio 3.29: Polygons

--- CAP3/Polygons/Polygons/src/Main.java
+++ CAP3/Polygons/Polygons/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int sidesNumber;
+        double sideLength;
+        double apothem;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor introduzca el numero de lados del poligono: ");
+        sidesNumber = scanner.nextInt();
+
+        Polygons polygons = new Polygons(sidesNumber);
+
+        if (sidesNumber < 3 || sidesNumber > 12) {
+            System.out.println(polygons.polyDen());
+        } else {
+            System.out.print("Por favor introduzca la longitud de los lados: ");
+            sideLength = scanner.nextDouble();
+            polygons.setSideLength(sideLength);
+
+            System.out.print("Por favor introduzca la apotema del poligono: ");
+            apothem = scanner.nextDouble();
+            polygons.setApothem(apothem);
+
+            System.out.println("RESULTADOS");
+            System.out.println("__________");
+            System.out.println("El Area del " + polygons.polyDen() + " es: " + polygons.areaCalc());
+            System.out.println("El Perimetro del " + polygons.polyDen() + " es: " + polygons.periCalc());
+        }
     }
 }
--- CAP3/Polygons/Polygons/src/Polygons.java
+++ CAP3/Polygons/Polygons/src/Polygons.java
@@ -1,9 +1,62 @@
 public class Polygons {
-    public double areaCalc(int sidesNumber, double sideLength, double apothem) {
+    private int sidesNumber;
+    private double sideLength;
+    private double apothem;
+    Polygons(int sidesNumber) {
+        this.sidesNumber = sidesNumber;
+    }
+    public double areaCalc() {
         return (sidesNumber * sideLength * apothem) / 2;
     }
 
     public double periCalc() {
+        return (sidesNumber * sideLength);
+    }
 
+    public String polyDen() {
+        String denomination;
+        switch (sidesNumber) {
+            case 3:
+                denomination = "Triangulo";
+                break;
+            case 4:
+                denomination = "Cuadrilatero";
+                break;
+            case 5:
+                denomination = "Pentagono";
+                break;
+            case 6:
+                denomination = "Hexagono";
+                break;
+            case 7:
+                denomination = "Heptagono";
+                break;
+            case 8:
+                denomination = "Octagono";
+                break;
+            case 9:
+                denomination = "Eneagono";
+                break;
+            case 10:
+                denomination = "Decagono";
+                break;
+            case 11:
+                denomination = "Endecagono";
+                break;
+            case 12:
+                denomination = "Dodecagono";
+                break;
+            default:
+                denomination = "Invalid Polygon";
+        }
+        return denomination;
+    }
+
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public void setApothem(double apothem) {
+        this.apothem = apothem;
     }
 }
