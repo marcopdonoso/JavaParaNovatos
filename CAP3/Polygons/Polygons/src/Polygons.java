f03ac98d7bfcb262fcd77f519e93ccfe5e64fa8f
Marco Perez Donoso <marcopdonoso@MacBook-Pro-de-Marco.local>
2022-07-05 11:15:45 -0400

    Ejercicio 3.29: Polygons

---
+++ CAP3/Polygons/Polygons/src/Polygons.java
@@ -0,0 +1,9 @@
+public class Polygons {
+    public double areaCalc(int sidesNumber, double sideLength, double apothem) {
+        return (sidesNumber * sideLength * apothem) / 2;
+    }
+
+    public double periCalc() {
+
+    }
+}
