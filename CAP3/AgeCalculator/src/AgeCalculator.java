6851f3f3c92b4d01cc561ce7d3239fd7a2688a77
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-06 11:41:10 -0400

    Ejercicio 3.32 Calculadora de edad Con errores de compilacion por haber camiado el Encoding Type, no se como revertir la situacion

---
+++ CAP3/AgeCalculator/src/AgeCalculator.java
@@ -0,0 +1,37 @@
+﻿public class AgeCalculator {
+    private final int birthYear;
+    private final int birthMonth;
+    private final int birthDay;
+    private final int currentYear;
+    private final int currentMonth;
+    private final int currentDay;
+
+    AgeCalculator(int birthYear, int birthMonth, int birthDay, int currentYear, int currentMonth, int currentDay) {
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+        this.currentYear = currentYear;
+        this.currentMonth = currentMonth;
+        this.currentDay = currentDay;
+    }
+
+    public int ageCalc() {
+        int ageInYears;
+        ageInYears = currentYear - birthYear;
+        if (currentMonth > birthMonth) {
+           return ageInYears;
+        } else if (currentMonth < birthMonth) {
+            ageInYears--;
+            return ageInYears;
+        } else {
+            if (currentDay > birthDay) {
+                return ageInYears;
+            } else if (currentDay < birthDay) {
+                ageInYears--;
+                return ageInYears;
+            } else {
+                return ageInYears;
+            }
+        }
+    }
+}
