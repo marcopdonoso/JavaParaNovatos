d7ab137ccdfaf4267ae0ff2f5c9571c92f40bb97
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-12 17:13:15 -0400

    Ejercicio 5.3: Loteria Primitiva

---
+++ CAP5/PrimitiveLottery/src/PrimitiveLottery.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PrimitiveLottery {
+    public static void main(String[] args) {
+        int a;
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            a = (int) (Math.random() * 50);
+            if (arrayList.contains(a)) {
+                i--;
+            } else {
+                arrayList.add(a);
+            }
+        }
+
+        for (Integer integer : arrayList) {
+            System.out.print(integer + " ");
+        }
+    }
+}
+
