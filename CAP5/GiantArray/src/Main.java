c9fd86a1aca035656d92fb876998efa46518b922
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-13 12:04:32 -0400

    Ejercicio 5.6 - 5.7: Medicion de tiempos de ejecucion de metodos de busqueda para comparar

--- CAP5/GiantArray/src/Main.java
+++ CAP5/GiantArray/src/Main.java
@@ -18,7 +18,11 @@ public class Main {
 
         System.out.print("Introduzca el número que desea buscar en Array: ");
         number = scanner.nextInt();
+        long start = System.nanoTime();
         int position = GiantArray.searchNumber(array,number);
+        long end = System.nanoTime();
+        long duration1 = end - start;
+
         if (position != -1) {
             System.out.println("El número está en la posición: " + position);
         } else {
@@ -29,12 +33,21 @@ public class Main {
         GiantArray.printArray(arrayList);
         System.out.print("Introduzca el número que desea buscar en ArrayList: ");
         number = scanner.nextInt();
+
+        start = System.nanoTime();
         position = GiantArray.searchNumberInArrayList(arrayList,number);
+        end = System.nanoTime();
+        long duration2 = end - start;
+
         if (position != -1) {
             System.out.println("El número está en la posición: " + position);
         } else {
             System.out.println("No se encontró el número en el arreglo.");
         }
+
+        System.out.println();
+        System.out.println("Tiempo de busqueda secuencial en Array convencional: " + duration1);
+        System.out.println("Tiempo de busqueda con metodo indexOf() en ArrayList: " + duration2);
 
     }
 }
