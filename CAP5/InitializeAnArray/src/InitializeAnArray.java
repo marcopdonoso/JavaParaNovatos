ffa3f5b3362d6153a9bfa9e470bbf4795c8d9e25
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-15 12:09:47 -0400

    Uso del metodo fill() de la clase Array para rellenar el arreglo, mejora al imprimir el arreglo usando impresion con formato

--- CAP5/InitializeAnArray/src/InitializeAnArray.java
+++ CAP5/InitializeAnArray/src/InitializeAnArray.java
@@ -1,16 +1,15 @@
+import java.util.Arrays;
+
 public class InitializeAnArray {
     int[] array = new int[100];
 
     public void initializer() {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = -1;
-        }
+        Arrays.fill(array, -1);
     }
 
     public void printArray() {
-        for (int i = 0; i < array.length - 1; i++) {
-            System.out.print(array[i] + ", ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("Posicion %3d: %3d\n",i + 1,array[i]);
         }
-        System.out.println(array[array.length - 1]);
     }
 }
