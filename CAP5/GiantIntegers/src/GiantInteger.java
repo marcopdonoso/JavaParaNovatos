e405f6f2462acfdce5b4fbbe8d2c4ac5f77f305c
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-18 14:17:09 -0400

    Ejercicio 5.11: Sobre el ejercicio de suma de enteros gigantes (Ejercicio 5.10), ampliamos la clase para que sea capaz de restar enteros gigantes

--- CAP5/GiantIntegers/src/GiantInteger.java
+++ CAP5/GiantIntegers/src/GiantInteger.java
@@ -28,7 +28,6 @@ public class GiantInteger {
         GiantInteger giantIntegerResult = new GiantInteger();
         int overflow = 0;
         int partialResult;
-
         for (int i = maxSize - 1; i >= 0 ; i--) {
             partialResult = giantNumber[i] + giantInteger.getDigit(i) + overflow;
             if (partialResult >= 10) {
@@ -40,6 +39,54 @@ public class GiantInteger {
             }
         }
         return giantIntegerResult;
+    }
+
+    public GiantInteger sus(GiantInteger subtrahend) {
+        GiantInteger diff = new GiantInteger();
+        char bigger = whoIsBigger(subtrahend);
+        int[] min;
+        int[] sub;
+        if (bigger == 'E') {
+            Arrays.fill(diff.giantNumber,0);
+            return diff;
+        } else {
+            if (bigger == '2') {
+                min = subtrahend.giantNumber.clone();
+                sub = giantNumber.clone();
+            } else {
+                min = giantNumber.clone();
+                sub = subtrahend.giantNumber.clone();
+            }
+            int buffer;
+            for (int i = maxSize - 1; i >= 0; i--) {
+                buffer = min[i] - sub[i];
+                if (buffer < 0) {
+                    int j = i - 1;
+                    while (min[j] == 0) {
+                        min[j] = 9;
+                        j--;
+                    }
+                    min[j]--;
+                    buffer = 10 + min[i] - sub[i];
+                }
+                diff.giantNumber[i] = buffer;
+            }
+        }
+        return diff;
+    }
+
+    public char whoIsBigger(GiantInteger giantInteger) {
+        char bigger = 'E';
+        int index = 0;
+        while (bigger == 'E' && index < 50) {
+            if (giantNumber[index] > giantInteger.getDigit(index)) {
+                bigger = '1';
+            } else if (giantNumber[index] < giantInteger.getDigit(index)) {
+                bigger = '2';
+            }
+            index++;
+        }
+        return bigger;
     }
 
     public int getDigit(int index) {
--- CAP5/GiantIntegers/src/Main.java
+++ CAP5/GiantIntegers/src/Main.java
@@ -9,14 +9,19 @@ public class Main {
 
         System.out.print("Introduzca un número de max. 50 dígitos: ");
         giantInteger1.readGiantNumber(scanner.next());
-        System.out.println();
         System.out.print("Introduzca otro número de max. 50 dígitos: ");
         giantInteger2.readGiantNumber(scanner.next());
         System.out.println();
 
-        GiantInteger giantIntegerR = giantInteger1.add(giantInteger2);
+        GiantInteger giantIntegerSum = giantInteger1.add(giantInteger2);
 
-        System.out.print("El resultado de la suma es: ");
-        giantIntegerR.printGiantNumber();
+        System.out.print("El resultado de la suma es:  ");
+        giantIntegerSum.printGiantNumber();
+        System.out.println();
+
+        GiantInteger giantIntegerDiff = giantInteger1.sus(giantInteger2);
+
+        System.out.print("El resultado de la resta es: ");
+        giantIntegerDiff.printGiantNumber();
     }
 }
