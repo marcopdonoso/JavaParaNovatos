764c9087fcdd6a30abed0201c959a610dfb83d08
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-19 11:37:29 -0400

    Modificiacion al ejercicio anterior para inicializar la matriz con valores distintos dependiendo de si la fila es par o impar

--- CAP5/InitializeSimpleMatrix/src/SimpleMatrix.java
+++ CAP5/InitializeSimpleMatrix/src/SimpleMatrix.java
@@ -6,7 +6,12 @@ public class SimpleMatrix {
     public void initializeMatrix() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = -1;
+                if (i % 2 == 0) {
+                    matrix[i][j] = -1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+
             }
         }
     }
@@ -14,7 +19,7 @@ public class SimpleMatrix {
     public void readMatrix() {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.printf("%3d",matrix[i][j]);
             }
             System.out.println();
         }
