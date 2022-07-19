2a754b4c51e2d727501707a50c5d591e7ce52881
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-19 11:31:37 -0400

    Ejercicio 5.14: Inicializar matriz simple

---
+++ CAP5/InitializeSimpleMatrix/src/SimpleMatrix.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SimpleMatrix {
+    private final int[][] matrix = new int[10][10];
+
+    public void initializeMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    public void readMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
