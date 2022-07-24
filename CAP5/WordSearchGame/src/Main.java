ca47a5ad933f23f0f2e8bce4c92bb10dbc38e24e
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-24 01:04:52 -0400

    Agrego los metodos para: Seleccionar para del listado, escoger la casilla donde inicia la ubicacion de la palabra en funcion a su posicion (hor, ver, diag), y los metodos para escribir los caracteres de la palabra en la sopa de letras en hor y ver

--- CAP5/WordSearchGame/src/Main.java
+++ CAP5/WordSearchGame/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        WordSearchGenerator wordSearchGenerator = new WordSearchGenerator();
+        char[][] matrix = wordSearchGenerator.generateRandomMatrix(10,"s");
+        for (char[] chars : matrix) {
+            for (char unChar : chars) {
+                System.out.printf("%3c",unChar);
+            }
+            System.out.println();
+        }
     }
 }
--- CAP5/WordSearchGame/src/WordSearchGenerator.java
+++ CAP5/WordSearchGame/src/WordSearchGenerator.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class WordSearchGenerator {
-    private String[] wordsList;
+    private final String[] wordsList;
     private char[][] wordSearchMatrix;
     private char[][] solutionMatrix;
 
@@ -27,9 +30,35 @@ public class WordSearchGenerator {
         wordsList[19] = "ARPA";
     }
 
-    private char[][] generateRandomMatrix(int size, char shape) {
+    public void generateWordSearchGame() {
+        int size;
+        int wordsQuan;
+        int position;
+        boolean direction;
+        int[] initialBox;
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Size of the larger side: ");
+        size = scanner.nextInt();
+        System.out.print("Square or Rectangular? (s) or (r): ");
+        String shape = scanner.nextLine();
+        System.out.print("How many words?: ");
+        wordsQuan = scanner.nextInt();
+        wordSearchMatrix = generateRandomMatrix(size,shape);
+        for (int i = 0; i < wordsQuan; i++) {
+            String word = selectWord();
+            position = (int) (Math.random() * 2);
+            direction = random.nextBoolean();
+            initialBox = setInitialBox(position,word.length());
+            
+        }
+    }
+
+    public char[][] generateRandomMatrix(int size, String shape) {
         char[][] randomMatrix;
-        if (shape == 'r') {
+        if (shape.equals("r")) {
             randomMatrix = new char[size / 2][size];
         } else {
             randomMatrix = new char[size][size];
@@ -42,4 +71,68 @@ public class WordSearchGenerator {
         return randomMatrix;
     }
 
+    public String selectWord() {
+        return wordsList[(int) (Math.random() * 19)];
+    }
+
+    public int[] setInitialBox(int position, int size) {
+        int[] initialBox = new int[2];
+        int limitRow;
+        int limitCol;
+        switch (position) {
+            case 0:
+                limitRow = wordSearchMatrix.length - 1;
+                limitCol = wordSearchMatrix[0].length - 1 - size;
+                break;
+            case 1:
+                limitRow = wordSearchMatrix.length - 1 - size;
+                limitCol = wordSearchMatrix[0].length - 1;
+                break;
+            case 2:
+                limitRow = wordSearchMatrix.length - 1 - size;
+                limitCol = wordSearchMatrix[0].length - 1 - size;
+                break;
+            default:
+                limitRow = wordSearchMatrix.length - 1;
+                limitCol = wordSearchMatrix[0].length - 1;
+                break;
+        }
+        initialBox[0] = (int) (Math.random() * limitRow);
+        initialBox[1] = (int) (Math.random() * limitCol);
+        return initialBox;
+    }
+
+    public void horizontal(String wordString, int row, int column, boolean direction) {
+        char[] wordChar = wordString.toCharArray();
+        int j = 0;
+        int k = wordChar.length - 1;
+        if (!direction) {
+            for (int i = column; i < column + wordChar.length; i++) {
+                wordSearchMatrix[row][i] = wordChar[j];
+                j++;
+            }
+        } else {
+            for (int i = column; i < column + wordChar.length; i++) {
+                wordSearchMatrix[row][i] = wordChar[k];
+                k--;
+            }
+        }
+    }
+
+    public void vertical(String wordString, int row, int column, boolean direction) {
+        char[] wordChar = wordString.toCharArray();
+        int j = 0;
+        int k = wordChar.length - 1;
+        if (!direction) {
+            for (int i = row; i < row + wordChar.length; i++) {
+                wordSearchMatrix[i][column] = wordChar[j];
+                j++;
+            }
+        } else {
+            for (int i = row; i < row + wordChar.length; i++) {
+                wordSearchMatrix[i][column] = wordChar[k];
+                k--;
+            }
+        }
+    }
 }
