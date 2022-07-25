5a2c9b25357e398bd755e5023dbf8281ff70cdfd
Marco Perez Donoso <marcopdonoso@gmail.com>
2022-07-25 13:32:40 -0400

    Agrego metodos para Invertir palabra, chequear disponibilidad de espacio en la matriz del juego, mejoras en los metodos de escritura horizontal y vertical de las palabras y agrego metodo para escritura en diagonal

--- CAP5/WordSearchGame/src/Main.java
+++ CAP5/WordSearchGame/src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         WordSearchGenerator wordSearchGenerator = new WordSearchGenerator();
-        char[][] matrix = wordSearchGenerator.generateRandomMatrix(10,"s");
-        for (char[] chars : matrix) {
-            for (char unChar : chars) {
-                System.out.printf("%3c",unChar);
-            }
-            System.out.println();
-        }
+        wordSearchGenerator.generateWordSearchGame();
     }
 }
--- CAP5/WordSearchGame/src/WordSearchGenerator.java
+++ CAP5/WordSearchGame/src/WordSearchGenerator.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class WordSearchGenerator {
     private final String[] wordsList;
     private char[][] wordSearchMatrix;
+    private boolean[][] availabilityMatrix;
     private char[][] solutionMatrix;
 
     public WordSearchGenerator() {
@@ -32,28 +34,75 @@ public class WordSearchGenerator {
 
     public void generateWordSearchGame() {
         int size;
+        String shape;
         int wordsQuan;
+        String word;
         int position;
         boolean direction;
         int[] initialBox;
+        char[] wordChar;
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.print("Size of the larger side: ");
+        System.out.print("Size of the larger side?: ");
         size = scanner.nextInt();
         System.out.print("Square or Rectangular? (s) or (r): ");
-        String shape = scanner.nextLine();
+        shape = scanner.next();
         System.out.print("How many words?: ");
         wordsQuan = scanner.nextInt();
         wordSearchMatrix = generateRandomMatrix(size,shape);
+        if (shape.equals("r")) {
+            availabilityMatrix = new boolean[size / 2][size];
+        } else {
+            availabilityMatrix = new boolean[size][size];
+        }
         for (int i = 0; i < wordsQuan; i++) {
-            String word = selectWord();
+
+            do {
+                word = selectWord();
+            } while (word.length() >= size);
+
             position = (int) (Math.random() * 2);
             direction = random.nextBoolean();
+
+            if (direction) {
+                word = reverseWord(word);
+            }
+
+            wordChar = word.toCharArray();
             initialBox = setInitialBox(position,word.length());
 
+            if (checkAvailability(wordChar,initialBox,position)) {
+                System.out.println(word);
+                switch (position) {
+                    case 0:
+                        horizontal(wordChar, initialBox);
+                        break;
+                    case 1:
+                        vertical(wordChar,initialBox);
+                        break;
+                    case 2:
+                        diagonal(wordChar,initialBox);
+                        break;
+                }
+            } else {
+                i--;
+            }
         }
+        showMatrix(wordSearchMatrix);
+
+        for (boolean[] booleans : availabilityMatrix) {
+            for (boolean anBoolean : booleans) {
+                if (anBoolean) {
+                    System.out.printf("%3d",1);
+                } else {
+                    System.out.printf("%3d",0);
+                }
+            }
+            System.out.println();
+        }
+
     }
 
     public char[][] generateRandomMatrix(int size, String shape) {
@@ -73,6 +122,11 @@ public class WordSearchGenerator {
 
     public String selectWord() {
         return wordsList[(int) (Math.random() * 19)];
+    }
+
+    public String reverseWord(String word) {
+        StringBuilder stringBuilder = new StringBuilder(word);
+        return stringBuilder.reverse().toString();
     }
 
     public int[] setInitialBox(int position, int size) {
@@ -102,41 +156,93 @@ public class WordSearchGenerator {
         return initialBox;
     }
 
-    public void horizontal(String wordString, int[] initialBox, boolean direction) {
-        char[] wordChar = wordString.toCharArray();
+    public void horizontal(char[] wordChar, int[] initialBox) {
         int row = initialBox[0];
         int column = initialBox[1];
-        int j = 0;
-        int k = wordChar.length - 1;
-        if (!direction) {
-            for (int i = column; i < column + wordChar.length; i++) {
-                wordSearchMatrix[row][i] = wordChar[j];
-                j++;
-            }
-        } else {
-            for (int i = column; i < column + wordChar.length; i++) {
-                wordSearchMatrix[row][i] = wordChar[k];
-                k--;
-            }
+        for (char c : wordChar) {
+            wordSearchMatrix[row][column] = c;
+            availabilityMatrix[row][column] = true;
+            column++;
         }
     }
 
-    public void vertical(String wordString, int[] initialBox, boolean direction) {
-        char[] wordChar = wordString.toCharArray();
+    public void vertical(char[] wordChar, int[] initialBox) {
         int row = initialBox[0];
         int column = initialBox[1];
-        int j = 0;
-        int k = wordChar.length - 1;
-        if (!direction) {
-            for (int i = row; i < row + wordChar.length; i++) {
-                wordSearchMatrix[i][column] = wordChar[j];
-                j++;
+        for (char c : wordChar) {
+            wordSearchMatrix[row][column] = c;
+            availabilityMatrix[row][column] = true;
+            row++;
+        }
+    }
+
+    public void diagonal(char[] wordChar, int[] initialBox) {
+        int row = initialBox[0];
+        int column = initialBox[1];
+        for (char c : wordChar) {
+            wordSearchMatrix[row][column] = c;
+            availabilityMatrix[row][column] = true;
+            row++;
+            column++;
+        }
+    }
+
+    public boolean checkAvailability(char[] wordChar, int[] initialBox, int position) {
+        boolean available = true;
+        int row = initialBox[0];
+        int column = initialBox[1];
+        int i, j, k;
+        switch (position) {
+            case 0:
+                i = 0;
+                while (available && i < wordChar.length) {
+                    if (availabilityMatrix[row][column]) {
+                        if (wordChar[i] != wordSearchMatrix[row][column]) {
+                            available = false;
+                        }
+                    } else {
+                        i++;
+                        column++;
+                    }
+                }
+                break;
+            case 1:
+                i = 0;
+                while (available && i < wordChar.length) {
+                    if (availabilityMatrix[row][column]) {
+                        if (wordChar[i] != wordSearchMatrix[row][column]) {
+                            available = false;
+                        }
+                    } else {
+                        i++;
+                        row++;
+                    }
+                }
+                break;
+            case 2:
+                i = 0;
+                while (available && i < wordChar.length) {
+                    if (availabilityMatrix[row][column]) {
+                        if (wordChar[i] != wordSearchMatrix[row][column]) {
+                            available = false;
+                        }
+                    } else {
+                        i++;
+                        row++;
+                        column++;
+                    }
+                }
+                break;
+        }
+        return available;
+    }
+
+    public void showMatrix(char[][] matrix) {
+        for (char[] chars : matrix) {
+            for (char anChar : chars) {
+                System.out.printf("%3c",anChar);
             }
-        } else {
-            for (int i = row; i < row + wordChar.length; i++) {
-                wordSearchMatrix[i][column] = wordChar[k];
-                k--;
-            }
+            System.out.println();
         }
     }
 }
